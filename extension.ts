ee7a192595f25d15bfa1a86c967a59c8bdc39747
Johannes Rieken <johannes.rieken@gmail.com>
2017-04-27 17:29:45 +0200

    fix potential issue, cleanup

--- extension.ts
+++ extension.ts
@@ -1,50 +1,50 @@
 
-import {ExtensionContext, TextDocument, Selection, Range, Position, commands, window} from 'vscode';
+import * as vscode from 'vscode';
 
-export function activate(context:ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 
-    context.subscriptions.push(commands.registerCommand('jrieken.backspaceLeft', backspace));
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('jrieken.backspaceLeft', backspace));
 }
 
 
-function backspace() {
+function backspace(editor: vscode.TextEditor) {
 
-    const editor = window.activeTextEditor;
-    if (!editor) {
-        return;
+    const { document, selections, options: { tabSize } } = editor;
+
+    if (typeof tabSize === 'number') {
+
+        let hasNewSelections = false;
+        const newSelections = selections.map(selection => {
+            if (!selection.isEmpty) {
+                return selection;
+            }
+
+            const { start } = selection; // since it's empty start is the same as 'active'
+            if (start.character === 0) {
+                return selection;
+            }
+
+            const line = document.lineAt(start);
+            if (line.firstNonWhitespaceCharacterIndex < start.character) {
+                return selection;
+            }
+
+            // check for n-space characters preceeding the caret
+            let match = /^\t*((?: )+)$/.exec(line.text.substr(0, start.character));
+            if (match) {
+                hasNewSelections = true;
+                let toRemove = (match[1].length % tabSize) || tabSize;
+                return new vscode.Selection(start.with(void 0, start.character - toRemove), start);
+            }
+
+            return selection;
+        });
+
+        // set the new (expanded) selections and run 'deleteLeft'
+        if (hasNewSelections) {
+            editor.selections = newSelections;
+        }
     }
 
-    const {document, selections, options} = editor;
-    let hasNewSelections = false;
-
-    const newSelections = selections.map(selection => {
-        if (!selection.isEmpty) {
-            return selection;
-        }
-
-        const {start} = selection; // since it's empty start is the same as 'active'
-        if (start.character === 0) {
-            return selection;
-        }
-
-        const line = document.lineAt(start);
-        if (line.firstNonWhitespaceCharacterIndex < start.character) {
-            return selection;
-        }
-
-        // check for n-space characters preceeding the caret
-        let match = /^\t*((?: )+)$/.exec(line.text.substr(0, start.character));
-        if (match) {
-            hasNewSelections = true;
-            let toRemove = (match[1].length % options.tabSize) || options.tabSize;
-            return new Selection(start.with(void 0, start.character - toRemove), start);
-        }
-    });
-
-    // set the new (expanded) selections and run 'deleteLeft'
-    if (hasNewSelections) {
-        editor.selections = newSelections;
-    }
-
-    return commands.executeCommand('deleteLeft');
+    return vscode.commands.executeCommand('deleteLeft');
 }
